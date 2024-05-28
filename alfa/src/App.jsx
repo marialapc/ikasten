5545f0a3dc28eeae4f771ddee2ed85640630bb5a
marialapc <marialopez.apc@gmail.com>
2024-05-28 18:21:45 +0200

    test

--- alfa/src/App.jsx
+++ alfa/src/App.jsx
@@ -1,32 +1,36 @@
-import { useState } from "react"
-import Guitar from "./components/Guitar"
-import Header from "./components/Header"
-import { db } from "./data/db"
-
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
 
 function App() {
-
-  const [data, setData] = useState(db)
+  const [data, setData] = useState(db);
 
   return (
     <>
-
-    <Header />
-    <main className="container-xl mt-5">
+      <Header />
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
         </div>
-    </main>
+      </main>
 
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
