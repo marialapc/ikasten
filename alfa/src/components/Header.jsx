30e5fe1bc48b393640c5db23c8abd8c822b20c3a
Maria <maparicio@create-store.com>
2024-05-31 09:34:09 +0200

    update cart price with use memo and reduce

--- alfa/src/components/Header.jsx
+++ alfa/src/components/Header.jsx
@@ -1,4 +1,11 @@
- export default function Header({cart}) {
+
+import { useMemo } from 'react'
+
+
+export default function Header({cart}) {
+
+    const isEmpty = useMemo( () => cart.length === 0, [cart])
+    const cartTotal = useMemo( () => cart.reduce( (total, item) => total + (item.quantity * item.price), 0 ), [cart] )
 
     return (
         <header className="py-5 header">
@@ -16,9 +23,10 @@
                         <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
-                            {cart.length === 0 ? (
+                            {isEmpty ? (
                                 <p className="text-center">El carrito esta vacio</p>
                             ) : (
+                                <>
                             <table className="w-100 table">
                                 <thead>
                                     <tr>
@@ -70,10 +78,13 @@
                                      ))}
                                 </tbody>
                             </table>
-                             )}
 
+                            <p className="text-end">Total pagar: 
+                            <span className="fw-bold">${cartTotal}</span></p>
 
-                            <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
+                            </>
+                        )}
+                        
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                         </div>
                     </div>
