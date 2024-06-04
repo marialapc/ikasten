5518f824e9a533adf2cabe7df0bbe7c3fd7d5878
Maria <maparicio@create-store.com>
2024-06-04 13:38:45 +0200

    increasing the quantity of orders

--- calculadora_propinas/src/hooks/useOrder.ts
+++ calculadora_propinas/src/hooks/useOrder.ts
@@ -5,8 +5,22 @@ export default function useOrder () {
     const [order, setOrder] = useState<OrderItem[]>([])
   
     const addItem = (item : menuItem) => {
-        console.log(item)
+
+        const itemExist = order.find(orderItem => orderItem.id === orderItem.id)
+        if(itemExist) {
+            const updatedOrder = order.map( orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity + 1} : 
+                orderItem
+            )
+            setOrder(updatedOrder)
+        } else {
+            const newItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
     }
+
+    console.log(order)
+
     return {
         addItem
     }
