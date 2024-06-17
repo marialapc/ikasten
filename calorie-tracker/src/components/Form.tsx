b524aaf2eaf140f2be55ab18db31733f93cc6fff
Maria <maparicio@create-store.com>
2024-06-17 08:07:21 +0200

    submit event

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 import {Activity } from "../types"
 import { categories } from "../data/categories"
 
@@ -24,11 +24,21 @@ export default function Form () {
 
     const isValidActivity = () => {
         const { name, calories } = activity 
-        return name.trim() != '' && calories > 0
+        return name.trim() !== '' && calories > 0
+    }
+
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        console.log('submit')
+
     }
 
     return (
-        <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <form className="space-y-5 bg-white shadow p-10 rounded-lg"
+              onSubmit={handleSubmit}
+        >
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoría:</label>
                 <select 
@@ -71,8 +81,10 @@ export default function Form () {
 
             <input 
                 type="submit"
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" 
-                value='Guardar comida o Guardar ejercicio'/>
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10" 
+                value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+                disabled={!isValidActivity()}
+                />
         </form>
     )
 }
