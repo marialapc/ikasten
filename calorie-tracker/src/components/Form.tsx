606ae9dcf4897b81a948c2e167090d6004f20906
Maria <maparicio@create-store.com>
2024-06-18 08:17:03 +0200

    dispatch

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,8 +1,13 @@
-import { useState, ChangeEvent, FormEvent } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
 import { categories } from "../data/categories"
 import type { Activity } from "../types"
+import { ActivityActions } from "../reducers/activityReducer"
 
-export default function Form () {
+type FormProps = {
+    dispatch: Dispatch<ActivityActions>
+}
+
+export default function Form({dispatch} : FormProps) {
 
     const [activity, setActivity] = useState<Activity>({
         category: 1,
@@ -31,7 +36,7 @@ export default function Form () {
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        console.log('submit')
+        dispatch({ type: "save-activity", payload: {newActivity: activity}})
 
     }
 
--- calorie-tracker/src/reducers/activityReducer.ts
+++ calorie-tracker/src/reducers/activityReducer.ts
@@ -21,4 +21,7 @@ export const activityReducer = (
     }
 
 
+    return state
+
+
 }
