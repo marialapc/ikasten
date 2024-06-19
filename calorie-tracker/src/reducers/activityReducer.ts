5e836a9365fada619052dd73ae7b58a1ac0a83c0
Maria <maparicio@create-store.com>
2024-06-19 08:51:12 +0200

    fill the form when edit

--- calorie-tracker/src/reducers/activityReducer.ts
+++ calorie-tracker/src/reducers/activityReducer.ts
@@ -4,7 +4,7 @@ export type ActivityActions =
 { type: 'save-activity', payload: { newActivity : Activity } } |
 { type: 'set-activeId', payload: { id : Activity['id'] } }
 
-type ActivityState = {
+export type ActivityState = {
     activities : Activity[],
     activeId: Activity['id']
 }
@@ -29,10 +29,9 @@ export const activityReducer = (
     }
 
     if(action.type === 'set-activeId') {
-
         return {
             ...state,
-            ActiveId: action.payload.id
+            activeId: action.payload.id
         }
     }
 
