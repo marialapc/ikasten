99182d47569603c13d34fa3724ae5c5da1871083
Maria <maparicio@create-store.com>
2024-06-19 08:04:04 +0200

    add an icon for editing

--- calorie-tracker/src/components/ActivityList.tsx
+++ calorie-tracker/src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import { Activity } from "../types"
 import { categories } from "../data/categories"
 import { useMemo } from "react"
+import { PencilSquareIcon } from '@heroicons/react/24/outline'
 
 type ActivityListProps = {
     activities: Activity[]
@@ -30,7 +31,11 @@ export default function ActivityList({activities} : ActivityListProps) {
 
                 </div>
 
-                <div>
+                <div className="flex gap-5 items-center">
+                    <button>
+                        <PencilSquareIcon className="h-8 w-8 text-gray-800"
+                        />
+                    </button>
 
                 </div>
 
