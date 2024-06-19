b8b9440aa176f6ddc40907b9b5b31904ac13a815
Maria <maparicio@create-store.com>
2024-06-19 08:10:51 +0200

    creating the action for edit

--- calorie-tracker/src/reducers/activityReducer.ts
+++ calorie-tracker/src/reducers/activityReducer.ts
@@ -1,14 +1,17 @@
 import { Activity } from "../types"
 
 export type ActivityActions = 
-{ type: 'save-activity', payload: { newActivity : Activity } }
+{ type: 'save-activity', payload: { newActivity : Activity } } |
+{ type: 'set-activeId', payload: { id : Activity['id'] } }
 
 type ActivityState = {
-    activities : Activity[]
+    activities : Activity[],
+    activeId: Activity['id']
 }
 
 export const initialState : ActivityState = {
-    activities: []
+    activities: [],
+    activeId: ''
 }
 
 export const activityReducer = (
@@ -25,6 +28,13 @@ export const activityReducer = (
         }
     }
 
+    if(action.type === 'set-activeId') {
+
+        return {
+            ...state,
+            ActiveId: action.payload.id
+        }
+    }
 
     return state
 
