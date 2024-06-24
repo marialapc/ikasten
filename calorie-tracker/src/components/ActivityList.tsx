2484476a99b9f7898fd4ea92df19beacc8d01ea1
Maria <maparicio@create-store.com>
2024-06-24 08:53:15 +0200

    add a mesagge

--- calorie-tracker/src/components/ActivityList.tsx
+++ calorie-tracker/src/components/ActivityList.tsx
@@ -15,11 +15,16 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
         (category: Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '')
         , [activities])
 
+        const isEmpyActivities = useMemo(() => activities.length === 0, [activities])
+
     return (
         <>
         <h2 className="text-4xl font-bold text-slate-600 text-center">Comida y Actividades</h2>
 
-        {activities.map( activity => (
+        {isEmpyActivities ? 
+            <p className="text-center my-5">No hay actividades aún...</p> : 
+
+         activities.map( activity => (
             <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                 <div className="space-y--2 relative">
                     <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'} `}>
@@ -51,6 +56,7 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
 
             </div>
         ))}
+    
         </>
     )
 }
